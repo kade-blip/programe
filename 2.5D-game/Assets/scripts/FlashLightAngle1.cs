c52598cce7c0332659a86e8bc8e51f9628aa74cd
kade <kadecummings22@gmail.com>
2021-06-15 14:24:56 +1000

    save state
    saving for safety

--- 2.5D-game/Assets/scripts/FlashLightAngle1.cs
+++ 2.5D-game/Assets/scripts/FlashLightAngle1.cs
@@ -15,7 +15,7 @@ public class FlashLightAngle1 : MonoBehaviour
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, layerMask))
         {
-            Vector3 normal = (raycastHit.point- transform.position).normalized; // this makes it so instead of going all over it goes strat to the mouse
+            Vector3 normal = (raycastHit.point- transform.position).normalized; //* this makes it so instead of going all over it goes strat to the mouse
 
             //transform.position = raycastHit.point;
             transform.rotation = Quaternion.FromToRotation(Vector3.forward, normal); 
