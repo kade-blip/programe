559ddb6b0a309b6f2fab0ca378ec574ebe47aad7
kade <kadecummings22@gmail.com>
2021-06-16 11:29:19 +1000

    GUI and flashlight update
    adding and making flashlight update

--- 2.5D-game/Assets/scripts/FlashLight.cs
+++ 2.5D-game/Assets/scripts/FlashLight.cs
@@ -7,37 +7,92 @@ public class FlashLight : MonoBehaviour
 {
     
     public Light Light;
+    bool isOn;
+    private int State;
     
-    
-
-
-
     void Start()
     {
         Light = GetComponent<Light>(); //its getting the component light 
-        Light.intensity = 10f; // seting the insial intesity of the light 
+        Light.intensity = 10f; // seting the insial intesity of the light
+        State = 0;
     }
 
     void Update()
     {
-       
-
-
-        if (Input.GetKeyUp(KeyCode.Space)) // key to turn on/off the light
+        Cases();
+        if (Input.GetKeyUp(KeyCode.Space) && isOn == true) // if space isnt pressed and light on
         {
-            Light.enabled = !Light.enabled; // this is making sure that when the key is pressed the action happens 
-           
+            State = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && isOn == true) // if space is pressed and light is on
+        {
+            State = 1;
+        }
+
+        else if (Input.GetKeyUp(KeyCode.Space) && isOn == false) // if space is pressed and light isnt on
+        {
+            State = 2;
+        }
+      
+        else if (Input.GetKeyDown(KeyCode.Space) && isOn == false) // if space isnt pressed and light is off
+        {
+            State = 3;
+            
+        }
+        if (isOn == true)
+        {
+            lightIntesityUpdate();
+        }
+
+        if(isOn == false)
+        {
+            lightIntesityStop();
         }
 
 
-        Light.intensity -= Time.deltaTime * 0.33f; // gets the intesity of the light and decress it over time and the 0.33 is the speed it decresses at 
-       
+
+
+
 
     }
+    void Cases()
+    {
+        switch(State)
+        {
+            case 0:
+                Light.enabled = true;
+                isOn = true;
+                
+                Debug.Log("working11");
+                break;
+               
+            case 1:
+                Light.enabled = false;
+                isOn = false;
+                
+                break;
+            case 2:
+                Light.enabled = false;
+                isOn = false;
+               
+                break;
+            case 3:
+                Light.enabled = true;
+                isOn = true;
+                
+                break;
+        }
+    }
 
-   
+   void lightIntesityUpdate()
+    {
+        Light.intensity -= Time.deltaTime * 0.33f; // gets the intesity of the light and decress it over time and the 0.33 is the speed it decresses at
+    }
 
-
+    void lightIntesityStop()
+    {
+        Light.intensity = Light.intensity;
+    }
 
 
 
---
+++ 2.5D-game/Assets/scripts/batteryhealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class batteryhealth : MonoBehaviour
+{
+
+    public Slider meterSlider;
+    public FlashLight fl;
+    public void UpdateMeter(float value)
+    {
+        meterSlider.value = value;
+    }
+
+     void Update()
+    {
+        meterSlider.value = fl.Light.intensity;
+        Debug.Log(meterSlider.value);
+    }
+
+}
